0a6a7486783fbf8fcc21641730473b5c7f565dab
Moses Palmér <mosespalmer@gmail.com>
2015-10-28 11:09:05 +0100

    Updated pynput.mouse.Controller.Button documentation

--- lib/pynput/mouse/_base.py
+++ lib/pynput/mouse/_base.py
@@ -25,9 +25,18 @@ class Controller(object):
     """
     class Button(enum.Enum):
         """The various buttons.
+
+        The actual values for these items differ between platforms. Some
+        platforms may have additional buttons, but these are guaranteed to be
+        present everywhere.
         """
+        #: The left button
         left = 1
+
+        #: The middle button
         middle = 2
+
+        #: The right button
         right = 3
 
     @property
