b13566bd890c4756709f8731220ca619f7230458
Moses Palmér <mosespalmer@gmail.com>
2015-12-28 23:23:32 +0100

    Added modifiers

--- tests/keyboard_controller_tests.py
+++ tests/keyboard_controller_tests.py
@@ -156,6 +156,66 @@ class KeyboardControllerTest(unittest.TestCase):
             collect(),
             'Failed to apply dead key')
 
+    def test_alt_pressed(self):
+        """Asserts that alt_pressed works"""
+        # We do not test alt_r, since that does not necessarily exist on the
+        # keyboard
+        for key in (
+                pynput.keyboard.Key.alt,
+                pynput.keyboard.Key.alt_l):
+            self.controller.press(key)
+            self.assertTrue(
+                self.controller.alt_pressed,
+                'alt_pressed was not set with %s down' % key.name)
+            self.controller.release(key)
+            self.assertFalse(
+                self.controller.alt_pressed,
+                'alt_pressed was incorrectly set')
+
+    def test_ctrl_pressed(self):
+        """Asserts that ctrl_pressed works"""
+        for key in (
+                pynput.keyboard.Key.ctrl,
+                pynput.keyboard.Key.ctrl_l,
+                pynput.keyboard.Key.ctrl_r):
+            self.controller.press(key)
+            self.assertTrue(
+                self.controller.ctrl_pressed,
+                'ctrl_pressed was not set with %s down' % key.name)
+            self.controller.release(key)
+            self.assertFalse(
+                self.controller.ctrl_pressed,
+                'ctrl_pressed was incorrectly set')
+
+    def test_shift_pressed(self):
+        """Asserts that shift_pressed works with normal presses"""
+        for key in (
+                pynput.keyboard.Key.shift,
+                pynput.keyboard.Key.shift_l,
+                pynput.keyboard.Key.shift_r):
+            self.controller.press(key)
+            self.assertTrue(
+                self.controller.shift_pressed,
+                'shift_pressed was not set with %s down' % key.name)
+            self.controller.release(key)
+            self.assertFalse(
+                self.controller.shift_pressed,
+                'shift_pressed was incorrectly set')
+
+    def test_shift_pressed_caps_lock(self):
+        """Asserts that shift_pressed is True when caps lock is toggled"""
+        self.controller.press(pynput.keyboard.Key.caps_lock)
+        self.controller.release(pynput.keyboard.Key.caps_lock)
+        self.assertTrue(
+            self.controller.shift_pressed,
+            'shift_pressed was not set with caps lock toggled')
+
+        self.controller.press(pynput.keyboard.Key.caps_lock)
+        self.controller.release(pynput.keyboard.Key.caps_lock)
+        self.assertFalse(
+            self.controller.shift_pressed,
+            'shift_pressed was not deactivated with caps lock toggled')
+
     def test_type_latin(self):
         """Asserts that type works for a Latin string"""
         self.assertInput(
