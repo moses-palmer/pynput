ceffd64bc6b682ba0acfc564cc2a7e68c543798f
Moses Palmér <moses@blockmastersecurity.com>
2016-02-04 11:59:38 +0100

    Added key translator for Windows

--- lib/pynput/_util/win32.py
+++ lib/pynput/_util/win32.py
@@ -15,7 +15,9 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
+import contextlib
 import ctypes
+import six
 import threading
 
 from ctypes import windll, wintypes
@@ -286,3 +288,224 @@ class ListenerMixin(object):
         listener was created based on the event.
         """
         raise NotImplementedError()
+
+
+class KeyTranslator(object):
+    """A class to translate virtual key codes to characters.
+    """
+    _AttachThreadInput = ctypes.windll.user32.AttachThreadInput
+    _GetForegroundWindow = ctypes.windll.user32.GetForegroundWindow
+    _GetKeyboardLayout = ctypes.windll.user32.GetKeyboardLayout
+    _GetKeyboardState = ctypes.windll.user32.GetKeyboardState
+    _GetWindowThreadProcessId = ctypes.windll.user32.GetWindowThreadProcessId
+    _MapVirtualKeyEx = ctypes.windll.user32.MapVirtualKeyExW
+    _ToUnicodeEx = ctypes.windll.user32.ToUnicodeEx
+
+    _MAPVK_VK_TO_VSC = 0
+    _MAPVK_VK_TO_CHAR = 2
+
+    def __init__(self):
+        self.__state = (ctypes.c_byte * 255)()
+        self._cache = {}
+        self._reinject_arguments = None
+
+    def __call__(self, vk, is_press):
+        """Converts a virtual key code to a string.
+
+        :param int vk: The virtual key code.
+
+        :param bool is_press: Whether this is a press. Because the *win32*
+            functions used to translate the key modifies internal kernel state,
+            some cleanup must be performed for key presses.
+
+        :return: parameters suitable for the :class:`pynput.keyboard.KeyCode`
+            constructor
+
+        :raises OSError: if a call to any *win32* function fails
+        """
+        # Get the keyboard state and layout
+        state, layout = self._get_state_and_layout()
+
+        # Get the scan code for the virtual key
+        scan = self._to_scan(vk, layout)
+
+        # Try to reuse the previous key in the cache
+        try:
+            if is_press:
+                return self._cache[vk]
+            else:
+                return self._cache.pop(vk)
+        except KeyError:
+            pass
+
+        # Get a string representation of the key
+        char, is_dead = self._to_char(vk, layout)
+        modified_char = self._to_char_with_modifiers(vk, scan, state, layout)
+
+        # Clear the keyboard state if the key was a dead key
+        if is_dead:
+            self._reset_state(vk, scan, layout)
+
+        # If the previous key handled was a dead key, we reinject it
+        if self._reinject_arguments:
+            self._reinject(*self._reinject_arguments)
+            self._reinject_arguments = None
+
+        # If the current key is a dead key, we store the current state to be
+        # able to reinject later
+        elif is_dead:
+            self._reinject_arguments = (
+                vk,
+                scan,
+                (ctypes.c_byte * 255)(*state),
+                layout)
+
+        # Otherwise we just clear any previous dead key state
+        else:
+            self._reinject_arguments = None
+
+        # Update the cache
+        self._cache[vk] = {
+            'char': modified_char or char,
+            'is_dead': is_dead,
+            'vk': vk}
+
+        return self._cache[vk]
+
+    def _get_state_and_layout(self):
+        """Returns the keyboard state and layout.
+
+        The state is read from the currently active window if possible. It is
+        kept in a cache, so any call to this method will invalidate return
+        values from previous invocations.
+
+        :return: the tuple ``(state, layout)``
+        """
+        # Get the state of the keyboard attached to the active window
+        with self._thread_input() as active_thread:
+            if not self._GetKeyboardState(ctypes.byref(self.__state)):
+                raise OSError(
+                    'GetKeyboardState failed: %d',
+                    ctypes.wintypse.get_last_error())
+
+        # Get the keyboard layout for the thread for which we retrieved the
+        # state
+        layout = self._GetKeyboardLayout(active_thread)
+
+        return (self.__state, layout)
+
+    def _to_scan(self, vk, layout):
+        """Retrieves the scan code for a virtual key code.
+
+        :param int vk: The virtual key code.
+
+        :param layout: The keyboard layout.
+
+        :return: the scan code
+        """
+        return self._MapVirtualKeyEx(
+            vk, self._MAPVK_VK_TO_VSC, layout)
+
+    def _to_char(self, vk, layout):
+        """Converts a virtual key by simply mapping it through the keyboard
+        layout.
+
+        This method is stateless, so any active shift state or dead keys are
+        ignored.
+
+        :param int vk: The virtual key code.
+
+        :param layout: The keyboard layout.
+
+        :return: the string representation of the key, or ``None``, and whether
+            was dead as the tuple ``(char, is_dead)``
+        """
+        # MapVirtualKeyEx will yield a string representation for dead keys
+        flags_and_codepoint = self._MapVirtualKeyEx(
+            vk, self._MAPVK_VK_TO_CHAR, layout)
+        if flags_and_codepoint:
+            return (
+                six.unichr(flags_and_codepoint & 0xFFFF),
+                bool(flags_and_codepoint & (1 << 31)))
+        else:
+            return (None, None)
+
+    def _to_char_with_modifiers(self, vk, scan, state, layout):
+        """Converts a virtual key by mapping it through the keyboard layout and
+        internal kernel keyboard state.
+
+        This method is stateful, so any active shift state and dead keys are
+        applied. Currently active dead keys will be removed from the internal
+        kernel keyboard state.
+
+        :param int vk: The virtual key code.
+
+        :param int scan: The scan code of the key.
+
+        :param layout: The keyboard layout.
+
+        :param state: The keyboard state.
+
+        :return: the string representation of the key, or ``None``
+        """
+        # This will apply any dead keys and modify the internal kernel keyboard
+        # state
+        out = (ctypes.wintypes.WCHAR * 5)()
+        count = self._ToUnicodeEx(
+            vk, scan, ctypes.byref(state), ctypes.byref(out),
+            len(out), 0, layout)
+
+        return out[0] if count > 0 else None
+
+    def _reset_state(self, vk, scan, layout):
+        """Clears the internal kernel keyboard state.
+
+        This method will remove all dead keys from the internal state.
+
+        :param int vk: The virtual key code.
+
+        :param int scan: The scan code of the key.
+
+        :param layout: The keyboard layout.
+        """
+        state = (ctypes.c_byte * 255)()
+        out = (ctypes.wintypes.WCHAR * 5)()
+        while self._ToUnicodeEx(
+                vk, scan, ctypes.byref(state), ctypes.byref(out),
+                len(out), 0, layout) < 0:
+            pass
+
+    def _reinject(self, vk, scan, state, layout):
+        """Reinjects the previous dead key.
+
+        This must be called if ``ToUnicodeEx`` has been called, and the
+        previous key was a dead one.
+
+        :param keyboard_layou: The keyboard layout.
+        """
+        out = (ctypes.wintypes.WCHAR * 5)()
+        self._ToUnicodeEx(
+            vk, scan, ctypes.byref(state), ctypes.byref(out),
+            len(out), 0, layout)
+
+    @contextlib.contextmanager
+    def _thread_input(self):
+        """Temporarily attaches the input handling of this thread to that of
+        the currently active window.
+
+        The context manager returns the ID of the thread to which the input
+        handling is attached. This is the ID of the current thread if attaching
+        failed.
+        """
+        remote_thread = self._GetWindowThreadProcessId(
+            self._GetForegroundWindow(),
+            None)
+        local_thread = GetCurrentThreadId()
+
+        if self._AttachThreadInput(local_thread, remote_thread, True):
+            try:
+                yield remote_thread
+            finally:
+                self._AttachThreadInput(local_thread, remote_thread, False)
+        else:
+            yield local_thread
