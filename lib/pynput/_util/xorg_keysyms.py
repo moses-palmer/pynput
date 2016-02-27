1d094b6223de412ba65cad59832f088c3bac834c
Moses Palmér <mosespalmer@gmail.com>
2016-02-27 23:06:38 +0100

    Changed license to LGPL

--- lib/pynput/_util/xorg_keysyms.py
+++ lib/pynput/_util/xorg_keysyms.py
@@ -1,18 +1,18 @@
 # coding: utf-8
-# Copyright 2016 Moses Palmér
+# Copyright (C) 2015-2016 Moses Palmér
 #
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option)
-# any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
 # details.
 #
-# You should have received a copy of the GNU General Public License along with
-# this program. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 SYMBOLS = {
     'dead_grave': (0xfe50, u'\u0300'),
