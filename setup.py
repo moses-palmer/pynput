eefa070ebd6d7a1113c00e7ac311e951e4bac37c
Moses Palmér <mosespalmer@gmail.com>
2016-10-21 12:43:03 +0200

    Added linter as setup dependency

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ RUNTIME_PACKAGES = [
 
 #: Additional requirements used during setup
 SETUP_PACKAGES = [
+    'setuptools-lint >=0.5',
     'sphinx >=1.3.1']
 
 #: Packages requires for different environments
