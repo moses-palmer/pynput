550ad178c9b1cee18e62f3b9157bdc74ff988b15
Moses Palmér <mosespalmer@gmail.com>
2017-04-21 18:19:06 +0200

    Marked mouse movement and scolling as potentially raising ValueError

--- lib/pynput/mouse/_base.py
+++ lib/pynput/mouse/_base.py
@@ -69,6 +69,8 @@ class Controller(object):
 
         :param int dy: The vertical scroll. The units of scrolling is
             undefined.
+
+        :raises ValueError: if the values are invalid, for example out of bounds
         """
         self._scroll(dx, dy)
 
@@ -93,6 +95,8 @@ class Controller(object):
         :param int x: The horizontal offset.
 
         :param int dy: The vertical offset.
+
+        :raises ValueError: if the values are invalid, for example out of bounds
         """
         self.position = tuple(sum(i) for i in zip(self.position, (dx, dy)))
 
