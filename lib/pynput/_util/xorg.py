8bdc083a9e54c22a652fd98076a9b656bd3d3db0
Moses Palmér <mosespalmer@gmail.com>
2018-02-12 21:47:21 +0100

    Implemented injected flag for Xorg

--- lib/pynput/_util/xorg.py
+++ lib/pynput/_util/xorg.py
@@ -467,7 +467,10 @@ class ListenerMixin(object):
         while data and len(data):
             event, data = self._EVENT_PARSER.parse_binary_value(
                 data, self._display_record.display, None, None)
-            self._handle(self._display_stop, event)
+
+            # TODO: Find a way to correctly set this value
+            injected = False
+            self._handle(self._display_stop, event, injected)
 
     def _initialize(self, display):
         """Initialises this listener.
@@ -479,7 +482,7 @@ class ListenerMixin(object):
         """
         pass
 
-    def _handle(self, display, event):
+    def _handle(self, display, event, injected):
         """The device specific callback handler.
 
         This method calls the appropriate callback registered when this
@@ -488,5 +491,7 @@ class ListenerMixin(object):
         :param display: The display being used.
 
         :param event: The event.
+
+        :param bool injected: Whether the event was injected.
         """
         pass
