4560e9ed5c7f5c5f835254aa70f2e89dbba05b6c
Moses Palmér <mosespalmer@gmail.com>
2019-12-18 19:28:14 +0100

    Corrected GlobalHotKeys constructor

--- lib/pynput/keyboard/__init__.py
+++ lib/pynput/keyboard/__init__.py
@@ -214,13 +214,15 @@ class GlobalHotKeys(Listener):
 
     :raises ValueError: if any hotkey description is invalid
     """
-    def __init__(self, hotkeys):
+    def __init__(self, hotkeys, *args, **kwargs):
         self._hotkeys = [
             HotKey(HotKey.parse(key), value)
             for key, value in hotkeys.items()]
-        super(Listener, self).__init__(
+        super(GlobalHotKeys, self).__init__(
             on_press=self._on_press,
-            on_release=self._on_release)
+            on_release=self._on_release,
+            *args,
+            **kwargs)
 
     def _on_press(self, key):
         """The press callback.
