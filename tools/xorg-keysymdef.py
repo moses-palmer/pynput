75c3bf9649e54cb1182b982a5539bcb7c4cf7e4c
Moses Palmér <mosespalmer@gmail.com>
2020-03-03 09:07:20 +0100

    Updated copyright

--- tools/xorg-keysymdef.py
+++ tools/xorg-keysymdef.py
@@ -178,7 +178,7 @@ def main():
     syms = sorted(list(definitions(sys.stdin.read().splitlines())))
     sys.stdout.write('''# coding: utf-8
 # pynput
-# Copyright (C) 2015-2019-%d Moses Palmér
+# Copyright (C) 2015-2020-%d Moses Palmér
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
