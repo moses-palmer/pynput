cedd5217f366283ae0bc59db6702a8a4778d62d1
Moses Palmér <mosespalmer@gmail.com>
2021-06-30 18:09:47 +0200

    Updated pyobjc-framework-Quartz to version 7.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ SETUP_PACKAGES = [
 #: Packages requires for different environments
 EXTRA_PACKAGES = {
     ':sys_platform == "darwin"': [
-        'pyobjc-framework-Quartz >=7.0'],
+        'pyobjc-framework-Quartz >=7.3'],
     ':"linux" in sys_platform': [
         'evdev >= 1.3',
         'python-xlib >= 0.17'],
